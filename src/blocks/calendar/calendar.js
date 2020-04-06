0d61c914848cca5bc9ae4669153741b49423d33b
Vova Maltsev <vladimir.maltsev99@mail.ru>
2020-04-06 17:26:10 +0300

    dropdawn(need script)

--- src/blocks/calendar/calendar.js
+++ src/blocks/calendar/calendar.js
@@ -45,6 +45,20 @@ import "./../../../node_modules/air-datepicker/dist/css/datepicker.min.css"
 
 
 
+function Createbut(){
+  var element = document.getElementById("button-place");
+  var btn = document.createElement('button');
+  btn.id = "buttonSave";
+
+  $(document).ready(function() {
+    $("#buttonSave").click(function(){
+        alert("Hello World!");
+    })
+  })
+  btn.innerHTML = "Button";
+  element.appendChild(btn);
+};
+
 $('#calendar_from').datepicker({ 
   onSelect: function (fd, d, picker) { 
     $("#calendar_from").val(fd.split("-")[0]);
@@ -53,10 +67,43 @@ $('#calendar_from').datepicker({
   range:true,
   prevHtml: '<i class="fas fa-arrow-left"></i>',
   nextHtml: '<i class="fas fa-arrow-right"></i>',
-  avTitles: {
-    days: 'MMyyyy'
+  navTitles: {
+    days: 'MM yyyy'
   },
-  startDate: new Date(),
+  Acceptbut: function(){
+    var datepicker_container = document.getElementById("datepickers-container");
+    var btn = document.createElement('button');
+    btn.id = "Accept_but";
+    btn.innerHTML = "Button";
+    datepicker_container.appendChild(btn);
+  },
   
+  clearButton: true,
 });
+
+  
+/*
+today: function () {
+  this.silent = true;
+  this.view = this.opts.minView;
+  this.silent = false;
+  this.date = new Date();
+
+  if (this.opts.todayButton instanceof Date) {
+      this.selectDate(this.opts.todayButton)
+  }
+},
+
+clear: function () {
+  this.selectedDates = [];
+  this.minRange = '';
+  thView]._render();
+  this._setInputVis.maxRange = '';
+  this.views[this.currentalue();
+  if (this.opts.onSelect) {
+      this._triggerOnChange()
+  }
+},*/
+
+
 
