5fbd1190cb5a8200257ef58988fe88cd4ca17cc1
Vova Maltsev <vladimir.maltsev99@mail.ru>
2020-05-06 02:15:38 +0300

    pagination script
    full pagination script done

--- src/blocks/pagination/pagination.js
+++ src/blocks/pagination/pagination.js
@@ -8,44 +8,10 @@ for (var i = 4; i <= 13; i++) {
     pagination_list[i].style.display = 'none';
 }
 
-let selected = pagination_list[0];
-selected.classList.add('pagination-selected'); //styles for first
-let count = Number(selected.innerHTML);
+const parentPagination = document.getElementsByClassName('pagination'); //container
+parentPagination[0].addEventListener('click', check_if);
 
-const next = document.getElementById('pagination-next');
-next.addEventListener('click', arrow_next);
-
-/*function arrow_next(event) {
-    selected = event.target;
-    count = Number(selected.innerHTML);
-
-    const parentPagination = selected.closest('.pagination');
-    const itemsArr = parentPagination.querySelectorAll('.pagination__list-item');
-
-    [...itemsArr].forEach((element) => {
-        element.classList.remove('pagination-selected');
-    });
-
-    pagination_list[count - 1].classList.add('pagination-selected');
-}*/
-
-const pagination = document.querySelector('.pagination__list');
-pagination.addEventListener('click', paginatorHandle);
-
-function paginatorHandle(event) {
-    selected = event.target;
-    const parentPagination = selected.closest('.pagination');
-
-    const itemsArr = parentPagination.querySelectorAll('.pagination__list-item');
-
-    [...itemsArr].forEach((element) => {
-        element.classList.remove('pagination-selected');
-    });
-
-    selected.closest('.pagination__list-item').classList.add('pagination-selected');
-
-    count = Number(selected.innerHTML); //counter page number
-
+function check_if() {
     if (count > 3) {
         pagination_dots[0].style.display = 'block';
     } else {
@@ -66,10 +32,12 @@ function paginatorHandle(event) {
     } else {
         pagination_arrows[1].style.display = 'block';
     } //show or hide last arrow
+}
 
-    [...itemsArr].forEach((element) => {
+function around_block() {
+    [...pagination_list].forEach((element) => {
         if (count >= 3 && count <= 14) {
-            [...itemsArr].forEach((element) => {
+            [...pagination_list].forEach((element) => {
                 element.style.display = 'none';
             });
             for (var i = count - 2; i <= count; i++) {
@@ -77,7 +45,7 @@ function paginatorHandle(event) {
             }
         } // block 2 around selected
         if (count == 15) {
-            [...itemsArr].forEach((element) => {
+            [...pagination_list].forEach((element) => {
                 element.style.display = 'none';
             });
             for (var i = count; i >= count - 2; i--) {
@@ -85,15 +53,69 @@ function paginatorHandle(event) {
             }
         } //styles if selected 15
         if (count == 1) {
-            [...itemsArr].forEach((element) => {
+            [...pagination_list].forEach((element) => {
                 element.style.display = 'none';
             });
             for (var i = count; i <= count + 3; i++) {
                 pagination_list[i - 1].style.display = 'block';
             }
         } //styles if selected 1
-
-        pagination_list[0].style.display = 'block'; //always show first
-        pagination_list[14].style.display = 'block'; //always show last
     });
+    pagination_list[0].style.display = 'block'; //always show first
+    pagination_list[14].style.display = 'block'; //always show last
+}
+
+function clear_classes() {
+    [...pagination_list].forEach((element) => {
+        //clear all
+        element.classList.remove('pagination-selected');
+    });
+}
+
+let selected = pagination_list[0];
+selected.classList.add('pagination-selected'); //styles for first
+let count = parseInt(selected.innerHTML); //counter = 1
+
+const pagination = document.querySelector('.pagination__list');
+pagination.addEventListener('click', paginatorHandle);
+
+function paginatorHandle(event) {
+    selected = event.target;
+
+    clear_classes();
+
+    selected.closest('.pagination__list-item').classList.add('pagination-selected');
+
+    count = parseInt(selected.innerHTML); //counter page number
+    around_block();
+}
+
+const next = document.getElementById('pagination-next');
+next.addEventListener('click', arrow_next);
+
+function arrow_next(event) {
+    //function for next arrow
+    count = parseInt(selected.innerHTML) + 1;
+
+    clear_classes();
+
+    pagination_list[count - 1].classList.add('pagination-selected');
+    selected = pagination_list[count - 1];
+
+    around_block();
+}
+
+const prev = document.getElementById('pagination-prev');
+prev.addEventListener('click', arrow_prev);
+
+function arrow_prev(event) {
+    //function for arrow back
+    count = parseInt(selected.innerHTML) - 1;
+
+    clear_classes();
+
+    pagination_list[count - 1].classList.add('pagination-selected');
+    selected = pagination_list[count - 1];
+
+    around_block();
 }
