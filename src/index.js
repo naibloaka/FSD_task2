487cacbd93e76356fed043712136d9ff975f1df0
Vova Maltsev <vladimir.maltsev99@mail.ru>
2020-05-23 15:29:02 +0300

    change page structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import './scss/main.scss';
 
-import './blocks/calendar/calendar.js';
-
-import './blocks/like-checkbox/like-checkbox.js';
-
+import './blocks/calendar/calendar.js'; // datepicker
+import './blocks/like-checkbox/like-checkbox.js'; // scripts for like button
 import './blocks/number-block/number-block.js';
-
-import './blocks/range-slider/range-slider.js';
-import './blocks/check-box_btn/check-box.js';
-
-import './blocks/pagination/pagination.js';
-
-import './blocks/dropdown/dropdown.js';
-import './blocks/star-rating/star-rating.js';
+import './blocks/range-slider/range-slider.js'; // scripts for price
+import './blocks/check-box_btn/check-box.js'; // hide or show checkbox
+import './blocks/pagination/pagination.js'; // pagination
+import './blocks/dropdown/dropdown.js'; // bad script REWORK
+import './blocks/star-rating/star-rating.js'; // star rating can change
--- webpack.config.js
+++ webpack.config.js
@@ -1,130 +1,130 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-	entry: {
-		app: "./src/index.js",
-	},
-	output: {
-		filename: "[name].js",
-		path: path.resolve(__dirname, "./dist"),
-		publicPath: "/dist",
-	},
-	devServer: {
-		contentBase: this.devServer,
-		overlay: true,
-	},
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist',
+  },
+  devServer: {
+    contentBase: this.devServer,
+    overlay: true,
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: "babel-loader",
-				exclude: "/node_modules/",
-			},
-			{
-				test: /\.pug$/,
-				loader: "pug-loader",
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader"],
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					"style-loader",
-					MiniCssExtractPlugin.loader,
-					{
-						loader: "css-loader",
-						options: { sourceMap: true },
-					},
-					{
-						loader: "sass-loader",
-						options: { sourceMap: true },
-					},
-				],
-			},
-			{
-				test: /\.(png|jpg|gif|svg)$/,
-				loader: "file-loader",
-				options: {
-					name: "[name].[ext]",
-				},
-			},
-			{
-				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-				use: [
-					{
-						loader: "file-loader",
-						options: {
-							name: "[name].[ext]",
-							outputPath: "fonts/",
-						},
-					},
-				],
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/',
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true },
+          },
+          {
+            loader: 'sass-loader',
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
+    ],
+  },
 
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: "[name].css",
-		}),
-		new webpack.ProvidePlugin({
-			$: "jquery",
-			jQuery: "jquery",
-			"window.jQuery": "jquery",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/pages/number-page.pug",
-			filename: "number-page.html",
-		}),
-		new CopyWebpackPlugin([
-			{ from: "./src/fonts", to: "./fonts" },
-			{ from: "./src/img", to: "./img" },
-		]),
-		new HtmlWebpackPlugin({
-			template: "./src/pages/welcome.pug",
-			filename: "welcome.html",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/pages/review-page.pug",
-			filename: "review-page.html",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/pages/register-page.pug",
-			filename: "register-page.html",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/pages/login-page.pug",
-			filename: "login-page.html",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/ui-kit/colors.pug",
-			filename: "colors.html",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/ui-kit/cards.pug",
-			filename: "cards.html",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/ui-kit/headers-and-footers.pug",
-			filename: "headers-and-footers.html",
-		}),
-		new HtmlWebpackPlugin({
-			template: "./src/ui-kit/Form-elements.pug",
-			filename: "Form-elements.html",
-		}),
-	],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/number-page/number-page.pug',
+      filename: 'number-page.html',
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/fonts', to: './fonts' },
+      { from: './src/img', to: './img' },
+    ]),
+    new HtmlWebpackPlugin({
+      template: './src/pages/welcome-page/welcome-page.pug',
+      filename: 'welcome.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/review-page/review-page.pug',
+      filename: 'review-page.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/register-page/register-page.pug',
+      filename: 'register-page.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/login-page/login-page.pug',
+      filename: 'login-page.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/ui-kit/colors.pug',
+      filename: 'colors.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/ui-kit/cards.pug',
+      filename: 'cards.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/ui-kit/headers-and-footers.pug',
+      filename: 'headers-and-footers.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/ui-kit/Form-elements.pug',
+      filename: 'Form-elements.html',
+    }),
+  ],
 
-	resolve: {
-		modules: ["node_modules"],
-		alias: {
-			"owl.carousel": "owl.carousel/dist/owl.carousel.min.js",
-		},
-	},
+  resolve: {
+    modules: ['node_modules'],
+    alias: {
+      'owl.carousel': 'owl.carousel/dist/owl.carousel.min.js',
+    },
+  },
 };
